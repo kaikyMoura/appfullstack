9254e921495978ae6ff088f2e2f7f6354c5bcf2c
kaiky de Moura <kaikymoura972@gmail.com>
2022-07-20 13:57:48 -0300

    Header created

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+/*import logo from '../../assets/img/logo.svg';*/
+import './styles.css';
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <h1>DSMeta</h1>
+                <p>Criado por: KaikyTupinambá</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
